05d203137b805ea42cec00716e84a259b9438bb8
Eunchong Yu <gracie@spoqa.com>
2019-03-24 03:26:57 +0900

    Add an example

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,76 @@
 //! with it at the high-level.
 //!
 //! [request-response pattern]: https://en.wikipedia.org/wiki/Request%E2%80%93response
+//!
+//! # Examples
+//!
+//! ```
+//! # #[cfg(feature = "reqwest")]
+//! # { (|| {
+//! use std::time::Duration;
+//!
+//! use adventure::prelude::*;
+//! use adventure::response::ResponseLocalFutureObj;
+//! use futures::Future;
+//! use reqwest::{r#async::Client, Error};
+//! use serde::Deserialize;
+//!
+//! // declare the request and the desired output.
+//! struct GetRepo<'a> {
+//!     owner: &'a str,
+//!     repo: &'a str,
+//! };
+//!
+//! #[derive(Deserialize)]
+//! struct Repository {
+//!     id: u64,
+//!     name: String,
+//!     full_name: String,
+//!     description: String,
+//! }
+//!
+//! // describe the relation of request, result, and error.
+//! impl BaseRequest for GetRepo<'_> {
+//!     type Ok = Repository;
+//!     type Error = Error;
+//! }
+//!
+//! impl<'a> RepeatableRequest<&'a Client> for GetRepo<'_> {
+//!     // convenient wrapper for boxed futures
+//!     type Response = ResponseLocalFutureObj<'a, Self::Ok, Self::Error>;
+//!
+//!     // implement how to send the request and extract the result.
+//!     fn send(&self, client: &'a Client) -> Self::Response {
+//!         let url = format!("https://api.github.com/repos/{}/{}", self.owner, self.repo);
+//!         let resp = client.get(&url)
+//!             .header("Accept", "application/vnd.github.v3+json")
+//!             .send()
+//!             .and_then(|mut r| r.json());
+//!         ResponseLocalFutureObj::new(resp)
+//!     }
+//! }
+//!
+//! // this is for `with_backoff()` method.
+//! impl RetriableRequest for GetRepo<'_> {
+//!     fn should_retry(&self, err: &Self::Error, next_duration: Duration) -> bool {
+//!         err.is_server_error()
+//!     }
+//! }
+//!
+//! // let's try it
+//!
+//! use tokio::runtime::current_thread::block_on_all;
+//!
+//! let client = Client::new();
+//! let request = GetRepo { owner: "spoqa", repo: "adventure" };
+//! let response = request
+//!     .with_backoff()
+//!     .into_response(&client);
+//! let repo = block_on_all(response.into_future())?;
+//! assert_eq!(repo.description, "Helps your great adventure for the various type of requests.");
+//! # Ok::<_, Box<dyn std::error::Error>>(())
+//! # })().unwrap(); }
+//! ```
 #![cfg_attr(feature = "std-future", feature(futures_api))]
 #![deny(rust_2018_idioms)]
 
