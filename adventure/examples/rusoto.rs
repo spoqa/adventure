182f768d261a2e27d4b194757b22f62210cad2f5
Eunchong Yu <gracie@spoqa.com>
2019-03-25 00:49:26 +0900

    Add examples in README.md

--- adventure/examples/rusoto.rs
+++ adventure/examples/rusoto.rs
@@ -1,48 +1,27 @@
-Adventure
-=========
-
-[![crates.io][b/crates/i]][b/crates] [![docs.rs][b/docs/i]][b/docs]
-
-[b/crates]: https://crates.io/crates/adventure
-[b/crates/i]: https://meritbadge.herokuapp.com/adventure
-[b/docs]: https://docs.rs/adventure/
-[b/docs/i]: https://docs.rs/adventure/badge.svg
-
-Provides general utilities for the web requests, like [exponential backoff][] and pagination.
-
-[exponential backoff]: https://en.wikipedia.org/wiki/Exponential_backoff
-
-
-Examples
---------
-
-```rust
+use std::env::args;
 use std::sync::Arc;
 
 use adventure::prelude::*;
-use futures::prelude::*;
-
 use adventure_rusoto_ecs::AwsEcs;
+use futures::prelude::*;
 use rusoto_core::Region;
 use rusoto_ecs::{EcsClient, ListServicesRequest};
 
 fn main() {
+    let args: Vec<_> = args().take(2).collect();
+    let cluster = args.get(1).cloned();
+
     let client = EcsClient::new(Region::default());
     let req = ListServicesRequest {
-        cluster: Some("MyEcsCluster".to_owned()),
+        cluster,
         ..Default::default()
     };
 
+    #[cfg(all(feature = "futures", not(feature = "std-future")))]
     tokio::run(
-
-        // prepare a request
         AwsEcs::from(req)
-            // backoff if server error is occured
             .retry()
-            // repeat to retrieve all results
             .paginate(Arc::new(client))
-
-            // compatible with futures
             .for_each(|page| {
                 for service in page.service_arns.unwrap_or_else(Vec::new) {
                     println!("{}", service);
@@ -55,4 +34,3 @@ fn main() {
             }),
     );
 }
-```
